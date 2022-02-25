370b211e32e5928e7af7dfcdeb5e4b297f5bf155
Viola8905 <viola89055@gmail.com>
2022-02-25 18:52:45 +0200

    initial commit (prepared environment, set up dependencies and connectyed to mongodb)

---
+++ server/index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(fileUpload({ useTempFiles: true }));
+
+// Connect to mongoDB
+
+const URI = process.env.MONGODB_URL;
+
+mongoose.connect(URI, {}, (err) => {
+  if (err) throw err;
+  console.log("Connected to MOngoDB");
+});
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server is running on the port ", PORT);
+});
